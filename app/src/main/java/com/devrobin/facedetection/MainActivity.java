f5e7bc3508f766afa4026e512a6ef796123704b5
Robin Das <developerrobindas@gmail.com>
2025-11-15 16:40:10 +0600

    add functionalities

--- app/src/main/java/com/devrobin/facedetection/MainActivity.java
+++ app/src/main/java/com/devrobin/facedetection/MainActivity.java
@@ -1,24 +1,91 @@
 package com.devrobin.facedetection;
 
+import android.app.ComponentCaller;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.FirebaseApp;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.FaceDetection;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnCamera;
+    private TextView textView;
+    private ImageView imageView;
+
+
+    private static final int REQUEST_CODE_PERMISSION = 12;
+    InputImage inputImage;
+    FaceDetection faceDetection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnCamera = findViewById(R.id.cameraBtn);
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.cameraImg);
+
+        FirebaseApp.initializeApp(this);
+
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFile();
+            }
         });
+
+    }
+
+    private void openFile() {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_CODE_PERMISSION);
+        }
+        else {
+            Toast.makeText(this, "Failed!!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data, @NonNull ComponentCaller caller) {
+        super.onActivityResult(requestCode, resultCode, data, caller);
+
+
+        Bundle bundle = data.getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("data");
+
+        FaceDetectionProcess(bitmap);
+        Toast.makeText(this, "Success!!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void FaceDetectionProcess(Bitmap bitmap) {
+
+
+
     }
 }
